579ff2c682cf1a8593c3b2032186e0f4a8947883
alstroud03 <alexandra@stroud-family.net>
2016-04-17 19:55:27 -0400

    Added files via upload

---
+++ Calendar.java
@@ -0,0 +1,64 @@
+package com.example.admin.budgethelper;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+public class Calendar extends AppCompatActivity {
+    CalendarView calendar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+
+        calendar = (CalendarView) findViewById(R.id.calendar);
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                Toast.makeText(getApplicationContext(), month + "/" + dayOfMonth + "/" + year, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.home:
+                Intent intent1 = new Intent(getApplicationContext(),HomePage.class);
+                startActivity(intent1);
+                return true;
+            case R.id.spending:
+                Intent intent2 = new Intent(getApplicationContext(),Spendings.class);
+                startActivity(intent2);
+                return true;
+            case R.id.updatebudget:
+                Intent intent3 = new Intent(getApplicationContext(),Budget.class);
+                startActivity(intent3);
+                return true;
+            case R.id.iouStartPage:
+                Intent intent4 = new Intent(getApplicationContext(), IOUStartPage.class);
+                startActivity(intent4);
+                return true;
+            case R.id.calendar:
+                Intent intent5 = new Intent(getApplicationContext(), Calendar.class);
+                startActivity(intent5);
+                return true;
+
+            default:
+                return true;
+        }
+    }
+}
---
+++ HomePage.java
@@ -0,0 +1,55 @@
+package com.example.admin.budgethelper;
+
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.home:
+                Intent intent1 = new Intent(getApplicationContext(),HomePage.class);
+                startActivity(intent1);
+                return true;
+            case R.id.spending:
+                Intent intent2 = new Intent(getApplicationContext(),Spendings.class);
+                startActivity(intent2);
+                return true;
+            case R.id.updatebudget:
+                Intent intent3 = new Intent(getApplicationContext(),Budget.class);
+                startActivity(intent3);
+                return true;
+            case R.id.iouStartPage:
+                Intent intent4 = new Intent(getApplicationContext(), IOUStartPage.class);
+                startActivity(intent4);
+                return true;
+            case R.id.calendar:
+                Intent intent5 = new Intent(getApplicationContext(), Calendar.class);
+                startActivity(intent5);
+                return true;
+
+            default:
+                return true;
+        }
+    }
+}
