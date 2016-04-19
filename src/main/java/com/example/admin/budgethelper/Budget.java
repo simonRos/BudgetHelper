501f84f85b177565bd3a9dfcd0bcd160f78ccf20
Simon <simon.h.rosner@gmail.com>
2016-04-19 18:22:55 -0400

    Added budgct object

--- src/main/java/com/example/admin/budgethelper/Budget.java
+++ src/main/java/com/example/admin/budgethelper/Budget.java
@@ -1,45 +1,29 @@
-package com.example.admin.budgethelper;
+package com.exampleandroid.testthing;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
-public class Budget extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_budget);
+/**
+ * Created by Simon on 4/19/2016.
+ */
+public class Budget {
+    //vars
+    private String username;
+    private double budget;
+    //constructor
+    public Budget(String owner, double amount){
+        username = owner;
+        budget = amount;
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    //setters
+    public void setUsername(String newUser){
+        username = newUser;
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        int id = item.getItemId();
-
-        switch (id){
-            case R.id.home:
-                Intent intent1 = new Intent(getApplicationContext(),HomePage.class);
-                startActivity(intent1);
-                return true;
-            case R.id.spending:
-                Intent intent2 = new Intent(getApplicationContext(),Spendings.class);
-                startActivity(intent2);
-                return true;
-            case R.id.updatebudget:
-                Intent intent3 = new Intent(getApplicationContext(),Budget.class);
-                startActivity(intent3);
-                return true;
-
-            default:
-                return true;
-        }
+    public void setBudget(double newBudget){
+        budget = newBudget;
+    }
+    //getters
+    public String getUsername(){
+        return username;
+    }
+    public double getBudget(){
+        return budget;
     }
 }
