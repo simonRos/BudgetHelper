7400dda07f87cf5b2983db1667a63f71900f81e5
Simon <simon.h.rosner@gmail.com>
2016-05-01 21:53:10 -0400

    Edited comments

--- src/main/java/com/example/admin/budgethelper/SpendingsEdit.java
+++ src/main/java/com/example/admin/budgethelper/SpendingsEdit.java
@@ -2,10 +2,13 @@
 CS 300
 BudgetHelper App
 May 1, 2016
+
+Allows the user to see a list of spendings.
+Allows user to delete spendings.
  */
 
 package com.example.admin.budgethelper;
-
+//Aimports
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.Intent;
@@ -24,12 +27,12 @@ import android.widget.TableRow;
 import android.widget.TableRow.LayoutParams;
 import android.widget.Toast;
 import android.database.Cursor;
+//Jimports
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 import java.text.DateFormat;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -51,8 +54,7 @@ public class SpendingsEdit extends AppCompatActivity {
         SQLiteDatabase db = myDbHelper.getReadableDatabase();
         Cursor c = db.rawQuery("SELECT " + DBContract.BudgetEntry.COLUMN_DATE + " FROM " + DBContract.BudgetEntry.TABLE_NAME, null);
         if(c.moveToLast()){
-            //do{
-            //assing values
+            //assign values
             dateStr = c.getString(0);
             //Do something Here with values
 
@@ -93,7 +95,6 @@ public class SpendingsEdit extends AppCompatActivity {
                 spendDateStr = c2.getString(1);
                 store = c2.getString(2);
                 id = c2.getInt(3);
-                //Do something Here with values
 
                 String expectedPattern = "MM/dd/yyyy";
                 SimpleDateFormat formatter = new SimpleDateFormat(expectedPattern);
@@ -181,9 +182,6 @@ public class SpendingsEdit extends AppCompatActivity {
         }
         c2.close();
         db2.close();
-
-        //spendingsList.setText(item);
-
     }
 
     @Override
@@ -193,7 +191,7 @@ public class SpendingsEdit extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
+    public boolean onOptionsItemSelected(MenuItem item){    //menu
         int id = item.getItemId();
 
         switch (id){
