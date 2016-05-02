7358852df3db08637f21d54b66bbacb9e470d75e
Simon <simon.h.rosner@gmail.com>
2016-05-01 21:21:47 -0400

    Added comments

--- src/main/java/com/example/admin/budgethelper/DBContract.java
+++ src/main/java/com/example/admin/budgethelper/DBContract.java
@@ -2,6 +2,10 @@
 CS 300
 BudgetHelper App
 May 1, 2016
+
+Database contract.
+Contains all variable names for database tables, rows, etc.
+This file is essentially a referance page. No real work is done here.
  */
 
 package com.example.admin.budgethelper;
@@ -16,73 +20,48 @@ public final class DBContract {
 
     public DBContract(){}
 
-
-    public static abstract class BudgetEntry implements BaseColumns {
+    public static abstract class BudgetEntry implements BaseColumns {   //Budget Table
         public static final String TABLE_NAME = "budget";
-        public static final String COLUMN_AMOUNT = "budgetamount";
-        public static final String COLUMN_DATE = "startdate";
+        public static final String COLUMN_AMOUNT = "budgetamount";  //amount col
+        public static final String COLUMN_DATE = "startdate";   //date col
     }
 
-    public static abstract class SpendingsEntry implements BaseColumns {
+    public static abstract class SpendingsEntry implements BaseColumns {    //Spendings Table
         public static final String TABLE_NAME = "spendings";
-        public static final String COLUMN_AMOUNT = "spendingamount";
-        public static final String COLUMN_STORE = "spendingstore";
-        public static final String COLUMN_DATE = "spendingdate";
+        public static final String COLUMN_AMOUNT = "spendingamount";    //amount col
+        public static final String COLUMN_STORE = "spendingstore";  //'store' col. store refers to the attached string used as a note by user
+        public static final String COLUMN_DATE = "spendingdate";    //date col
     }
 
-    public static abstract class PersonEntry implements BaseColumns {
-        //Person Table
+    public static abstract class PersonEntry implements BaseColumns {   //person table
         public static final String TABLE_NAME = "person";
         public static final String COLUMN_USERNAME = "username";
         public static final String COLUMN_PASSWORD = "password";
     }
 
-    public static abstract class IOUEntry implements BaseColumns {
-        //IOU table
+    public static abstract class IOUEntry implements BaseColumns { //IOU table
         public static final String TABLE_NAME = "IOU";
-        //public static final String COLUMN_USER = "IOU_Username";
-        //public static final String ID = "iou_ID";
-        public static final String COLUMN_DESC = "description";
-        public static final String COLUMN_FROM = "From_Name";
-        public static final String COLUMN_AMOUNT = "amount";
-        public static final String COLUMN_DATE = "date";
+        public static final String COLUMN_DESC = "description"; //description col. User note
+        public static final String COLUMN_FROM = "From_Name";   //From col
+        public static final String COLUMN_AMOUNT = "amount";    //amount col
+        public static final String COLUMN_DATE = "date";    //date col
     }
 
-    public static abstract class UOEntry implements BaseColumns {
-        //IOU table
+    public static abstract class UOEntry implements BaseColumns {   //IOU table
         public static final String TABLE_NAME = "UO";
-        //public static final String COLUMN_USER = "IOU_Username";
-        //public static final String ID = "iou_ID";
-        public static final String COLUMN_DESC = "description";
-        public static final String COLUMN_TO = "To_Name";
-        public static final String COLUMN_AMOUNT = "amount";
-        public static final String COLUMN_DATE = "date";
+        public static final String COLUMN_DESC = "description"; //Description col. User note
+        public static final String COLUMN_TO = "To_Name";   //To col
+        public static final String COLUMN_AMOUNT = "amount";    //amount col
+        public static final String COLUMN_DATE = "date";    //date col
     }
 
-    public static abstract class ReminderEntry implements BaseColumns {
-        //Reminder
+    //Note: reminders were not implemented but we left the table in the database
+    //If we ever decide to revisit the app and add a reminder feature, we would need this table
+    public static abstract class ReminderEntry implements BaseColumns { //Reminder table
         public static final String TABLE_NAME = "reminder";
-        public static final String COLUMN_IOUID = "reminder_iouID";
-        public static final String COLUMN_MESSAGE = "message";
-        public static final String COLUMN_DATE = "date";
+        public static final String COLUMN_IOUID = "reminder_iouID"; //Reminders need unique IDs
+        public static final String COLUMN_MESSAGE = "message";  //Message col. User note
+        public static final String COLUMN_DATE = "date";    //date col
     }
-/*
-    public static abstract class SpendingsEntry implements BaseColumns {
-        //Spendings
-        public static final String TABLE_NAME = "spendings";
-        public static final String COLUMN_STORE = "spendstore";
-        //public static final String SPENDINGS_ID = "spendings_ID";
-        public static final String COLUMN_AMOUNT = "spendingamount";
-        public static final String COLUMN_DATE = "spendingdate";
-    }
-/*
-    public static abstract class BudgetEntry implements BaseColumns {
-        //WeeklyBudget
-        public static final String budgetTable = "weekly_budget";
-        //public static final String budgetUser = "budget_user";
-        public static final String weeklyBudget = "budget_amount";
-        public static final String startDate = "budget_date";
-    }
-*/
 }
 
