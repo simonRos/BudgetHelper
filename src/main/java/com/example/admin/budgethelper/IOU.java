d52d46355bddc2974f6ce60f34af27991fa6685b
Simon <simon.h.rosner@gmail.com>
2016-05-01 21:39:01 -0400

    Added comments

--- src/main/java/com/example/admin/budgethelper/IOU.java
+++ src/main/java/com/example/admin/budgethelper/IOU.java
@@ -2,9 +2,12 @@
 CS 300
 BudgetHelper App
 May 1, 2016
+
+IOU page.
+Allows users to create IOUs.
  */
 package com.example.admin.budgethelper;
-
+//imports
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.ContentValues;
@@ -22,11 +25,11 @@ import android.widget.Toast;
 
 public class IOU extends AppCompatActivity {
 
-    Button save,ioudate;
-    EditText amount,name, dateInput;
+    Button save,ioudate;    //date and save buttons
+    EditText amount,name, dateInput;    //edit fields for amount, name, and date
     String amountVal, nameVal, dateInputVal;
-
-    int year_x,month_x,day_x;
+    int year_x,month_x,day_x;   //date
+    
     static final int DIALOG_ID = 0;
 
     @Override
@@ -34,46 +37,38 @@ public class IOU extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iou);
 
-        final java.util.Calendar cal = java.util.Calendar.getInstance();
-        year_x = cal.get(java.util.Calendar.YEAR);
-        month_x = cal.get(java.util.Calendar.MONTH);
-        day_x = cal.get(java.util.Calendar.DAY_OF_MONTH);
+        final java.util.Calendar cal = java.util.Calendar.getInstance();    //get java standard cal
+        year_x = cal.get(java.util.Calendar.YEAR); //current year
+        month_x = cal.get(java.util.Calendar.MONTH);    //current month
+        day_x = cal.get(java.util.Calendar.DAY_OF_MONTH);   //current day
 
         showDialogOnButtonClick();
 
-        amount=(EditText)findViewById(R.id.amount);
-        name=(EditText)findViewById(R.id.name);
-        dateInput=(EditText)findViewById(R.id.dateInput);
+        amount=(EditText)findViewById(R.id.amount); //amount
+        name=(EditText)findViewById(R.id.name); //name
+        dateInput=(EditText)findViewById(R.id.dateInput);   //date
 
-        save=(Button)findViewById(R.id.saveButton);
+        save=(Button)findViewById(R.id.saveButton); //save button
 
         save.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-
-                /*
-                bhDatabase myDB = new bhDatabase(getApplicationContext());
-                SQLiteDatabase userDB = myDB.getWritableDatabase();
-                BudgetDB myBudget = new BudgetDB("example", 100);
-                        //Double.parseDouble(amount));
-                userDB.createBudget(myBudget);
-                */
-
-
+            public void onClick(View v) {   //on Save click
+            
                 amountVal = ""+amount.getText();
                 nameVal = ""+amount.getText();
                 dateInputVal = ""+dateInput.getText();
 
-                if(amountVal.equals("") || nameVal.equals("") || dateInputVal.equals("")){
+                if(amountVal.equals("") || nameVal.equals("") || dateInputVal.equals("")){  //ensure fields are full
                     String errormsg = "Please fill out all fields";
                     int duration=Toast.LENGTH_LONG;
-                    Toast toast=Toast.makeText(getApplicationContext(), errormsg, duration);
+                    Toast toast=Toast.makeText(getApplicationContext(), errormsg, duration);    //error toast
                     toast.show();
-                }else {
+                }else { 
                     DBHelper myDbHelper = new DBHelper(getApplicationContext());
                     SQLiteDatabase db = myDbHelper.getWritableDatabase();
                     ContentValues values = new ContentValues();
-
+                    
+                    //insert values into db
                     values.put(DBContract.IOUEntry.COLUMN_AMOUNT, amount.getText().toString());
                     values.put(DBContract.IOUEntry.COLUMN_FROM, name.getText().toString());
                     values.put(DBContract.IOUEntry.COLUMN_DATE, dateInput.getText().toString());
@@ -84,18 +79,18 @@ public class IOU extends AppCompatActivity {
                             values);
 
                     String result;
-
+                    //Check to see if db successfully update with new IOU
                     if (newRowId != -1) {
                         result = "IOU Updated";
                     } else {
                         result = "Error creating IOU";
                     }
 
-
                     int duration = Toast.LENGTH_LONG;
                     Toast toast = Toast.makeText(getApplicationContext(), result, duration);
                     toast.show();
-
+                    
+                    //reset fields
                     amount.setText("");
                     name.setText("");
                     dateInput.setText("");
@@ -127,15 +122,13 @@ public class IOU extends AppCompatActivity {
         return null;
     }
 
-    private DatePickerDialog.OnDateSetListener dpickerListner
+    private DatePickerDialog.OnDateSetListener dpickerListner   //datePicker
             = new DatePickerDialog.OnDateSetListener(){
         @Override
         public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth){
             year_x = year;
             month_x = monthOfYear + 1;
             day_x = dayOfMonth;
-            //Toast.makeText(IOU.this, month_x + "/" + day_x + "/" + year_x, Toast.LENGTH_SHORT).show();
-            //ioudate.setText(month_x + "/" + day_x + "/" + year_x);
             dateInput.setText(month_x+"/"+day_x+"/"+year_x);
 
         }
@@ -148,7 +141,7 @@ public class IOU extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
+    public boolean onOptionsItemSelected(MenuItem item){    //menu
         int id = item.getItemId();
 
         switch (id){
